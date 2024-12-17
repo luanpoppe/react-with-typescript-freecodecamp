4cabe69cb0bca380e6f94e6108c263efc33a9cf5
luanpoppe <luanpoppe@gmail.com>
2024-12-17 20:21:39 -0300

    feat: adicionando estilizações iniciais e primeiros componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { InputField } from "./components/InputField";
+import { Todo } from "./model";
 
 // function App() {
 //   return <div>Hello world</div>;
 // }
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  function handleAdd(e: React.FormEvent) {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo("");
+    }
+  }
+
   return (
     <div className="App">
       <span className="heading">Taskify</span>
 
-      <InputField />
+      <InputField
+        todo={todo}
+        setTodo={setTodo}
+        handleAdd={(e) => handleAdd(e)}
+      />
     </div>
   );
 };
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,7 +1,33 @@
-export function InputField() {
+import { useRef } from "react";
+import "./style.css";
+
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+export function InputField({ todo, setTodo, handleAdd }: Props) {
+  const inputRef = useRef<HTMLInputElement>(null);
   return (
-    <form className="input">
-      <input type="input" placeholder="Enter a task" className="input__box" />
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
+      <input
+        ref={inputRef}
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        type="input"
+        placeholder="Enter a task"
+        className="input__box"
+      />
+      <button type="submit" className="input_submit">
+        Go
+      </button>
     </form>
   );
 }
